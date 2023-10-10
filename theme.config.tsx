0a3bfaff1fbd7b0589865390bc28662ef8354c47
Timmy Jose <zoltan.jose@gmail.com>
2023-10-10 15:56:03 +0530

    Changes:   - migrating from `mintlify` to `Nextra`.   - created basic skeletal structure from  the`Polylang` site as     template.   - migrating the pages.

---
+++ theme.config.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  primaryHue: 314,
+  project: {
+    link: 'https://github.com/polybase/docs',
+  },
+  chat: {
+    link: 'https://discord.com/invite/DrXkRpCFDX',
+  },
+  docsRepositoryBase: 'https://github.com/polybase/docs',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Polybase DB by Polybase Labs'
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Polybase DB" />
+      <meta property="og:description" content="" />
+      <meta property="og:image" content="" />
+      <meta name="twitter:site" content="@polybase_xyz"></meta>
+      <meta property="twitter:image" content="" />
+    </>
+  ),
+  footer: {
+    text: 'Polybase DB Docs',
+  },
+}
+
+export default config
